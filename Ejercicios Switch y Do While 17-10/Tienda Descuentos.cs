a23252b15a83d48b1794c5a71741240b6527c00d
AlbertoCalzada <113586207+AlbertoCalzada@users.noreply.github.com>
2022-10-24 23:59:36 +0200

    Update Tienda Descuentos.cs

--- Ejercicios Switch y Do While 17-10/Tienda Descuentos.cs
+++ Ejercicios Switch y Do While 17-10/Tienda Descuentos.cs
@@ -26,22 +26,33 @@ namespace TiendaPromociones
         {
             Console.WriteLine("¿Cuánto cuesta este producto sin descuento?");
             double price = int.Parse(Console.ReadLine());
-            double finalprice;
-             double doscount = 0.2;
+            double finalprice = 0;
+            double discount = 0.2;
             for (int i=1;i<=numproducts ; i=i+1) 
             {
                 for (int j = 1; j <= numdiscounts; j = j + 1)
                 {
                     
-                    if (j<=numdiscounts )
+                    if (j<=numdiscounts && i==1 )
                     {
-
+                        
                         Console.WriteLine("20% descuento");
-
+                        finalprice = finalprice + price; 
+                        Console.WriteLine(finalprice);
+                    } else if(j <= numdiscounts && i == 2)
+                    {
+                        discount = discount / 2;
+                        Console.WriteLine("10%descuento");
+                    }else if (j <= numdiscounts && i == 3)
+                    {
+                        discount = discount / 2;
+                        Console.WriteLine("5%descuento");
                     }
-                    
 
-                    Console.WriteLine("segundo bucle");
+
+
+
+                        Console.WriteLine("segundo bucle");
                     
                 }
                 Console.WriteLine("primer bucle");
