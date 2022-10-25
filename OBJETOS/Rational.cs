236f1f4e2fed7ee5b12d8901e9eef6f44f594b66
AlbertoCalzada <113586207+AlbertoCalzada@users.noreply.github.com>
2022-10-25 17:33:12 +0200

    Create Rational.cs

---
+++ OBJETOS/Rational.cs
@@ -0,0 +1,77 @@
+using System;
+
+namespace Rational
+{
+    
+
+
+    class Rational
+    {
+
+        int numerator;
+        int denominator;
+
+
+        public Rational()
+        {
+            denominator = 1;
+            numerator = 1;
+        }
+
+        public Rational(int newnumerator, int newdenominator)
+        {
+            numerator = newnumerator;
+            denominator = newdenominator;
+
+
+        }
+
+        public void SetSum()
+        {
+            numerator = newnumerator;
+            denominator = newdenominator;
+
+        }
+        public int GetSum()
+        {
+            int result = numerator / denominator;
+            return result;
+
+        }
+        public void Subtraction()
+        {
+
+
+        }
+        public void Multiplication()
+        {
+
+
+        }
+        public void Division()
+        {
+
+
+        }
+        public void Comparison()
+        {
+
+
+        }
+
+        public void Write()
+        {
+
+
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
