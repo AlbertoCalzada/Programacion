955e3b98f17c0865c218e957ac255e43d8a57620
AlbertoCalzada <113586207+AlbertoCalzada@users.noreply.github.com>
2022-10-26 10:11:14 +0200

    Update Rational.cs

--- OBJETOS/Rational.cs
+++ OBJETOS/Rational.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Rational
 {
-    
+
 
 
     class Rational
@@ -14,7 +14,7 @@ namespace Rational
 
         public Rational()
         {
-            denominator = 1;
+            denominator = 0;
             numerator = 1;
         }
 
@@ -26,52 +26,86 @@ namespace Rational
 
         }
 
-        public void SetSum()
-        {
-            numerator = newnumerator;
-            denominator = newdenominator;
+       
+        //public Rational Add(Rational r2)
+        //{
+            
 
-        }
-        public int GetSum()
-        {
-            int result = numerator / denominator;
-            return result;
-
-        }
+        //}
         public void Subtraction()
         {
 
 
         }
-        public void Multiplication()
+        public Rational Multiplication(Rational r2)
         {
-
+            int newnum = numerator * r2.numerator;
+            int newden = denominator * r2.denominator;
+            Rational result = new Rational(newnum, newden);
+            return result;
+        }
+        public Rational Divide(Rational r2)
+        {
+            int newnum = numerator * r2.denominator;
+            int newden = denominator * r2.numerator;
+            Rational result = new Rational(newnum, newden);
+            return result;
 
         }
-        public void Division()
+        public bool Compare(Rational r2)
         {
-
-
-        }
-        public void Comparison()
-        {
-
-
-        }
-
-        public void Write()
-        {
-
-
+            float num1F = (float)numerator;
+            float den1F = (float)denominator;
+            float num2F = (float)r2.numerator;
+            float den2F = (float)r2.denominator;
+            
+            float decimal1 = num1F / den1F;
+            float decimal2 = num2F / den2F;
+            bool result = decimal1 == decimal2;
+            return result;
         }
 
+        public string Write() //ToString
+        {
+            string result= numerator + "/" + denominator;
+            return result;
+        }
+        public void Print() 
+        {
+            Console.WriteLine(numerator + "/" + denominator); 
+            
+        }
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            Rational r0;
+            r0 = new Rational();
+            Rational r1;
+            r1 = new Rational(1,2);
+            Rational r2;
+            r2 = new Rational(2, 3);
+            Rational r3 = new Rational(2, 4);
+            Rational rMult;
+            Rational rDiv;
 
+            rMult = r1.Multiplication(r2);
+            bool b1=r1.Compare(r2); //FALSE
+            bool b2=r1.Compare(r3); //TRUE
+            rDiv=r1.Divide(r2);
+            
+            string s = r1.Write();
+            r1.Print();
+
+            Console.WriteLine("el resultado de la multiplicacion es " + rMult.Write());
+            Console.WriteLine("el resultado de la DIVISON es " + rDiv.Write());
+            Console.WriteLine(r1.Write()+ "==" + r2.Write() + "=" + b1);
+            Console.WriteLine(r1.Write() + "==" + r3.Write() + "=" + b2);
+            Console.WriteLine("llamando a to string" +s);
+
+            Console.WriteLine("Fin");
         }
     }
 }
