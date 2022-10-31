fd48119c6a64d7d9b3ecb3d50320c483a807e82b
AlbertoCalzada <113586207+AlbertoCalzada@users.noreply.github.com>
2022-10-31 13:25:42 +0100

    Update Rectangle.cs

--- OBJETOS/Rectangle.cs
+++ OBJETOS/Rectangle.cs
@@ -1,143 +1,124 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Rectangle
+namespace Ejercicio_Clases3
 {
+    class Point
+    {
+        int X;
+        int Y;
 
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Point(int newcoorX, int newcoorY)
+        {
+            X = newcoorX;
+            Y = newcoorY;
+        }
+
+        public void SetX(int newX)
+        {
+            X = newX;
+        }
+        public void SetY(int newY)
+        {
+            Y = newY;
+        }
+
+        public int GetX()
+        {
+            return X;
+        }
+
+        public int GetY()
+        {
+            return Y;
+        }
+
+        public string toString()
+        {
+            string result = ("" + X + ", " + Y + "");
+            return result;
+        }
+    }
 
     class Rectangle
     {
-        Point tr;
-        Point tl;
-        Point br;
-        Point bl;
+        Point pointone;
+        Point pointtwo;
+        Point pointthree;
+        Point pointfour;
 
-        public Rectangle(Point tr, Point tl, Point br, Point bl)
+        public Rectangle(Point pointone, Point pointtwo, Point pointthree, Point pointfour)
         {
-
-            this.tr = tr;
-            this.tl = tl;
-            this.br = br;
-            this.bl = bl;
-
-
-
+            this.pointone = pointone;
+            this.pointtwo = pointtwo;
+            this.pointthree = pointthree;
+            this.pointfour = pointfour;
+        }
+        public Rectangle(Point pointone, Point pointtwo, Point pointthree)
+        {
+            this.pointone = pointone;
+            this.pointtwo = pointtwo;
+            this.pointthree = pointthree;
+            this.pointfour = new Point();
+        }
+        public double PerimeterCalc(double largeside, double shortside)
+        {
+            double result = largeside * 2 + shortside * 2;
+            return result;
         }
 
-        public Rectangle(Point tr, Point tl, Point br)
+        public int Area()
         {
-
-
-            this.tr = tr;
-            this.tl = tl;
-            this.br = br;
-            bl = new Point();
-
-
-        }
-
-        public float GetArea()
-        {
-            int rectbase = br.GetX() - bl.GetX();
-            int rectHeight = tr.GetY() - br.GetY();
-
-            return rectbase *rectHeight;
-
+            int rectBase = pointthree.GetX() - pointfour.GetX();
+            int rectHeight = pointone.GetX() - pointthree.GetX();
+            int result = rectBase * rectHeight;
+            return result;
 
         }
 
         public string toString()
         {
-
-            string result = "(" + x + "," + y + " )";
-            return result;
+            string pointTL = ("" + pointone.GetX() + ", " + pointone.GetY() + "" + pointtwo.GetX() + ", " + pointtwo.GetY() +
+                "" + pointthree.GetX() + ", " + pointthree.GetY() + "" + pointfour.GetX() + ", " + pointfour.GetY());
+            return pointTL;
         }
 
-
-
+        public void ViewDates()
+        {
+            Console.WriteLine("El primer punto es: " + pointone);
+            Console.WriteLine("El segundo punto es: " + pointtwo);
+            Console.WriteLine("El tercer punto es: " + pointthree);
+            Console.WriteLine("El cuarto punto es: " + pointfour);
+        }
     }
-
-    class Point
-    {
-
-        int x;
-        int y;
-
-
-        public Point()
-        {
-
-            x = 0;
-            y = 0;
-
-        }
-
-        public Point(double x, double y)
-        {
-
-            this. x = x;
-            this.y = y;
-
-        }
-
-        public void SetX(int newx)
-        {
-
-            x = newx;
-        }
-
-        public int GetX()
-        {
-
-            return x;
-        }
-
-        public void SetY(int newy)
-        {
-
-            y = newy;
-        }
-
-        public int GetY()
-        {
-
-            return y;
-        }
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Point p00 = new Point(0, 0);
+            Point p00 = new Point();
             Point p11 = new Point(1, 1);
             Point p13 = new Point(1, 3);
             Point p33 = new Point(3, 3);
             Point p31 = new Point(3, 1);
 
-            Point p52= new Point(5, 2);
+            Point p52 = new Point(5, 2);
             Point p50 = new Point(5, 0);
             Point p02 = new Point(0, 2);
 
             Rectangle rect1 = new Rectangle(p33, p13, p31, p11);
-            Rectangle rect2 = new Rectangle(p52, p02, p50);
+            Rectangle rect2 = new Rectangle(p02, p52, p50);
 
-            Console.WriteLine("Rectangulo 1 : " + rect1.toString());
-            Console.WriteLine("Rectangulo 2 : " + rect2.toString());
-
-            Console.WriteLine("fin");
+            Console.WriteLine("Rectangulo 1:\n " + rect1.toString());
+            Console.WriteLine("Rectangulo 2:\n " + rect2.toString());
         }
-
-        // añadir metodo dibujar esto con asteriscos
     }
 }
