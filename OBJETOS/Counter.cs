4657e7e624105a8bee21f9a0eae8ea4897ce243e
AlbertoCalzada <113586207+AlbertoCalzada@users.noreply.github.com>
2022-11-08 09:35:37 +0100

    Update Counter.cs

--- OBJETOS/Counter.cs
+++ OBJETOS/Counter.cs
@@ -7,40 +7,65 @@ namespace Contador
     {
         static void Main(string[] args)
         {
-            Counter C1 = new Counter();
-            Counter C2 = new Counter(5);
-            Counter C3 = new Counter(C2);
+            Counter c1 = new Counter();
+            Counter c2 = new Counter(5);
+            Counter c3 = new Counter(c2);
 
-            C1.setcounter(2);
-            C1.increase();
-            C1.increase();
-            Console.WriteLine(C1.getcounter());
-            C1.decrease();
-            Console.WriteLine(C1.getcounter());
+            c1.setcounter(2);
+            c1.increase();
+            c1.increase();
+            Console.WriteLine(c1.getcounter());
+            c1.decrease();
+            Console.WriteLine(c1.getcounter());
             Console.WriteLine();
-            C2.increase();
-            C2.decrease();
-            C2.decrease();
-            Console.WriteLine(C2.getcounter());
+            c2.increase();
+            c2.decrease();
+            c2.decrease();
+            Console.WriteLine(c2.getcounter());
             Console.WriteLine();
-            Console.WriteLine(C3.getcounter());
+            Console.WriteLine(c3.getcounter());
+
+            
+
         }
     }
 
     class Counter
     {
         int counter;
+        int add;
+
+        int repetitions;
         public Counter()
         {
             counter = 0;
+            add = 1;
+            repetitions = 1;
         }
-        public Counter(int counter)
+        public Counter(int counter, int add, int repetitions)
         {
             this.counter = counter;
+            this.add = add;
+            this.repetitions = repetitions;
         }
-        public Counter(Counter C3)
+
+        public Counter(int counter, int add)
         {
-            this.counter = C3.counter;
+            this.counter = counter;
+            this.add = add;
+            repetitions = 1;
+        }
+        public Counter(int add)
+        {
+            counter = 0;
+            this.add = add;
+            repetitions = 1;
+        }
+        public Counter(Counter copy) //constructor copia
+        {
+            this.counter = copy.counter;
+            this.add = copy.add;
+            this.repetitions = copy.repetitions;
         }
         public int getcounter()
         {
@@ -50,13 +75,82 @@ namespace Contador
         {
             this.counter = counter;
         }
-        public void increase()
+
+        public int getadd()
         {
-            counter = counter + 1;
+            return add;
         }
-        public void decrease()
+
+        public int getrepetitions()
         {
-            counter = counter - 1;
+            return repetitions;
+        }
+        public void setadd(int add)
+        {
+            this.add = add;
+        }
+
+        public void setrepetitions(int repetitions)
+        {
+            this.repetitions = repetitions;
+        }
+
+
+        public int increase()
+        {
+            counter = counter + add;
+            return counter;
+        }
+        public void Run()
+        {
+            for (int i = 0; i < repetitions; i = i + 1)
+            {
+                increase();
+            }
+        }
+        public void Run(int repetitions)
+        {
+            for (int i = 0; i < repetitions; i = i + 1)
+            {
+                increase();
+            }
+        }
+        public int increase(int amount)
+        {
+            counter = counter + amount;
+            return counter;
+        }
+        public int decrease()
+        {
+            counter = counter - add;
+            return counter;
+        }
+        public int decrease(int amount)
+        {
+            counter = counter - amount;
+            return counter;
+        }
+        public void Runbackwards()
+        {
+            for (int i = 0; i < repetitions; i = i + 1)
+            {
+                decrease();
+            }
+        }
+        public void Runbackwards(int repetitions)
+        {
+            for (int i = 0; i < repetitions; i = i + 1)
+            {
+                decrease();
+            }
+        }
+
+        public string tostring()
+        {
+
+            return "contador" + counter + " + " + add + "/" + repetitions;
+
         }
     }
 }
+
