d7b7a6168a9220dc93136b8df09b2dfed5ab2e1a
AlbertoCalzada <113586207+AlbertoCalzada@users.noreply.github.com>
2022-11-08 10:18:44 +0100

    Update Fecha.cs

--- OBJETOS/Fecha.cs
+++ OBJETOS/Fecha.cs
@@ -2,19 +2,19 @@ using System;
 
 namespace Fecha
 {
-     class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Date d1 = new Date();
-            Date d2 = new Date(17,10,1960);
+            Date d2 = new Date(17, 10, 1960);
 
             d2.setDay(21);
             d2.setMonth(11);
             d2.setYear(2020);
-            Console.WriteLine(d2.Tostring() +"\n"+ d2.checkDate());
-            d2.increaseDate();
-            Console.WriteLine(d2.Tostring());
+            Console.WriteLine(d2.toString() + "\n" + d2.checkDate());
+            d2.nextDay();
+            Console.WriteLine(d2.toString());
         }
     }
 
@@ -25,9 +25,10 @@ namespace Fecha
         int year;
         public Date()
         {
-            day = 0;
-            month = 0;
-            year= 0;
+            DateTime today = DateTime.Today; //esto es una genialidad , la programacion is very easy
+            day = today.Day;
+            month = today.Month;
+            year = today.Year;
         }
         public Date(int day, int month, int year)
         {
@@ -47,7 +48,7 @@ namespace Fecha
         {
             return year;
         }
-        
+
         public void setDay(int day)
         {
             this.day = day;
@@ -60,29 +61,107 @@ namespace Fecha
         {
             this.year = year;
         }
-        public string Tostring()
+        public string toString()
         {
             return "es el dia " + day + " del mes " + month + " del año " + year;
         }
 
         public bool checkDate()
         {
-            if(day>0 && day<31 &&month>1 && month<=12 && year>0 && year <= 5000)
-            {
-                return true;
-            }
-            else
+            
+
+            if(month<=0 && month > 12)
             {
                 return false;
             }
+            if(day<0 && day > 31)
+            {
+                return false;
+            }
+            
+            switch(month)
+            {
+                //meses de 30 dias
+                case 4: if (day == 31) return false; break;
+                case 6: if (day == 31) return false; break;
+                case 9: if (day == 31) return false; break;
+                case 11: if (day == 31) return false; break;
+                //meses de 31
+                case 1: 
+                case 3: 
+                case 5: 
+                case 7: 
+                case 8: 
+                case 10:
+                case 12:
+                    if (day == 31) return true; break;
+                
+                case 2: 
+                    if(IsLeap()==true)
+                    {
+                        if (day>29) //es bisiesto
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            if (day > 28) //no lo es
+                            {
+                                return false;
+                            }
+
+                        }
+                    }
+                    break;
+
+            }
+            return true;
         }
 
-        public void increaseDate()
+        public bool IsLeap() //bisiesto
+        {
+
+            return (year % 400 == 0) || (year % 4==0 && year % 100 != 0);
+        }
+
+        public void nextDay()
         {
             day = day + 1;
+            if(month==11 || month == 4 || month == 6 || month == 9) //30 dias
+            {
+                if (day >= 31)
+                {
+                    day = 1;
+                    month = month + 1;
+                }
+
+            } else s
+            if (month == 2)
+            {
+                if (day > 29) //es bisiesto
+                {
+                    day = day - 29;
+                    month = month + 1;
+                }
+                else
+                {
+                    if (day > 28) //no lo es
+                    {
+                        day = day - 28;
+                        month = month + 1;
+                    }
+
+                }
+            }
+            if (month > 12)
+            {
+
+                month = month - 12;
+                year = year + 1;
+            }
 
         }
 
-        
+
     }
 }
