d31d5ef1bdf4898f773010d63b79042d4388f390
AlbertoCalzada <113586207+AlbertoCalzada@users.noreply.github.com>
2022-11-09 09:44:40 +0100

    Update Fecha.cs

--- OBJETOS/Fecha.cs
+++ OBJETOS/Fecha.cs
@@ -1,167 +1,337 @@
+
 using System;
 
-namespace Fecha
+namespace Book
 {
-    class Program
+    using System;
+
+    namespace Libro
     {
-        static void Main(string[] args)
+        class Program
         {
-            Date d1 = new Date();
-            Date d2 = new Date(17, 10, 1960);
-
-            d2.setDay(21);
-            d2.setMonth(11);
-            d2.setYear(2020);
-            Console.WriteLine(d2.toString() + "\n" + d2.checkDate());
-            d2.nextDay();
-            Console.WriteLine(d2.toString());
-        }
-    }
-
-    class Date
-    {
-        int day;
-        int month;
-        int year;
-        public Date()
-        {
-            DateTime today = DateTime.Today; //esto es una genialidad , la programacion is very easy
-            day = today.Day;
-            month = today.Month;
-            year = today.Year;
-        }
-        public Date(int day, int month, int year)
-        {
-            this.day = day;
-            this.month = month;
-            this.year = year;
-        }
-        public int getDay()
-        {
-            return day;
-        }
-        public int getMonth()
-        {
-            return month;
-        }
-        public int getYear()
-        {
-            return year;
-        }
-
-        public void setDay(int day)
-        {
-            this.day = day;
-        }
-        public void setMonth(int month)
-        {
-            this.month = month;
-        }
-        public void setYear(int year)
-        {
-            this.year = year;
-        }
-        public string toString()
-        {
-            return "es el dia " + day + " del mes " + month + " del año " + year;
-        }
-
-        public bool checkDate()
-        {
-            
-
-            if(month<=0 && month > 12)
+            static void Main(string[] args)
             {
-                return false;
-            }
-            if(day<0 && day > 31)
-            {
-                return false;
-            }
-            
-            switch(month)
-            {
-                //meses de 30 dias
-                case 4: if (day == 31) return false; break;
-                case 6: if (day == 31) return false; break;
-                case 9: if (day == 31) return false; break;
-                case 11: if (day == 31) return false; break;
-                //meses de 31
-                case 1: 
-                case 3: 
-                case 5: 
-                case 7: 
-                case 8: 
-                case 10:
-                case 12:
-                    if (day == 31) return true; break;
-                
-                case 2: 
-                    if(IsLeap()==true)
-                    {
-                        if (day>29) //es bisiesto
-                        {
-                            return false;
-                        }
-                        else
-                        {
-                            if (day > 28) //no lo es
-                            {
-                                return false;
-                            }
+                Book b1 = new Book("123");
+                Book b2 = new Book("51341413");
+                Date today = new Date();
+                Date tomorrow = new Date();
+                tomorrow.nextDay();
 
-                        }
-                    }
-                    break;
+                Console.WriteLine(b1.toString());
+                Console.WriteLine(b2.toString());
+
+                b1.Loan();
+                Console.WriteLine(b1.toString());
+                b1.Return();
+                Console.WriteLine(b1.toString());
 
             }
-            return true;
         }
 
-        public bool IsLeap() //bisiesto
+        class Book
         {
+            string isbn;
+            string author;
+            int pags;
+            Date loandate;
+            Date returndate;
+           
 
-            return (year % 400 == 0) || (year % 4==0 && year % 100 != 0);
-        }
-
-        public void nextDay()
-        {
-            day = day + 1;
-            if(month==11 || month == 4 || month == 6 || month == 9) //30 dias
+            public Book()
             {
-                if (day >= 31)
+                isbn = "";
+                author = "";
+                pags = 1;
+                loandate = null;
+                returndate = null;
+            }
+            public Book(string isbn, string author, int pags, Date loandate, Date returndate)
+            {
+                this.isbn = isbn;
+                this.author = author;
+                this.pags = pags;
+                this.loandate = loandate;
+                this.returndate = returndate;
+            }
+            public Book(string isbn)
+            {
+                this.isbn = isbn;
+                isbn = "";
+                author = "";
+                loandate = null;
+                returndate = null;
+                pags = 1;
+            }
+            public Book(string isbn, int pags)
+            {
+                this.isbn = isbn;
+                isbn = "";
+                author = "";
+                loandate = null;
+                returndate = null;
+                this.pags = pags;
+            }
+
+            public bool Loan() 
+            {
+               
+                if((loandate==null) || (loandate != null && returndate != null))
                 {
-                    day = 1;
-                    month = month + 1;
+
+                    loandate = new Date();
+                    returndate = null;
+                    return true;
+                }else
+                {
+                    return false;
                 }
-
-            } else s
-            if (month == 2)
+                
+            }
+            public bool Loan(int year, int month, int day)
             {
-                if (day > 29) //es bisiesto
+                loandate = new Date(day, month, year);
+                if (loandate.checkDate() != true)
                 {
-                    day = day - 29;
-                    month = month + 1;
+                    loandate = null;
+                    return false;
+                }
+                return true;
+            }
+            public bool Return()
+            {
+                if(loandate!=null && returndate == null)
+                {
+                    returndate = new Date();
+                    
+                    return true;
                 }
                 else
                 {
-                    if (day > 28) //no lo es
+                    return false;
+                }
+            }
+            public bool IsLoaned()
+            {
+                return loandate != null && returndate == null;
+            }
+            public bool IsAvailable()
+            {
+                return loandate == null || (loandate != null && returndate != null);
+            }
+            public bool Return(int year, int month, int day)
+            {
+                returndate = new Date(day, month, year);
+                if (returndate.checkDate() != true)
+                {
+                    loandate = null;
+                    return false;
+                }
+                return true;
+            }
+            public string toString()
+            {
+                if (loandate == null)
+                {
+                    return "el autor " + author + " con el isbn " + isbn + " con el numero de paginas " + pags + " .Nunca se ha prestado";
+
+                }
+                else
+                {
+                    if (returndate == null)
                     {
-                        day = day - 28;
+                        return "el autor " + author + " con el isbn " + isbn + " con el numero de paginas " + pags + " Se prestó en la fecha "
+                            + loandate.toString() + " y no se ha devuelto";
+                    }
+                    else
+                    {
+                        return "el autor " + author + " con el isbn " + isbn + " con el numero de paginas " + pags + " Se prestó en la fecha "
+                                                + loandate.toString() + " y se devolvio en la fecha " + returndate.toString();
+                    }
+
+
+
+                }
+                
+            }
+            public string getIsbn()
+            {
+                return isbn;
+            }
+
+            public string getAuthor()
+            {
+                return author;
+            }
+
+            public int getPags()
+            {
+                return pags;
+            }
+
+            public Date getloanDate()
+            {
+                return loandate;
+            }
+            public Date getreturndate()
+            {
+                return returndate;
+            }
+            public void setloanDate(Date loandate)
+            {
+                this.loandate = loandate;
+            }
+            public void setreturnDate(Date returndate)
+            {
+                this.returndate = returndate;
+            }
+        }
+        class Date
+        {
+            int day;
+            int month;
+            int year;
+            public Date()
+            {
+                DateTime today = DateTime.Today; //esto es una genialidad , la programacion is very easy
+                day = today.Day;
+                month = today.Month;
+                year = today.Year;
+            }
+            public Date(int day, int month, int year)
+            {
+                this.day = day;
+                this.month = month;
+                this.year = year;
+            }
+            public int getDay()
+            {
+                return day;
+            }
+            public int getMonth()
+            {
+                return month;
+            }
+            public int getYear()
+            {
+                return year;
+            }
+
+            public void setDay(int day)
+            {
+                this.day = day;
+            }
+            public void setMonth(int month)
+            {
+                this.month = month;
+            }
+            public void setYear(int year)
+            {
+                this.year = year;
+            }
+            public string toString()
+            {
+                return "es el dia " + day + " del mes " + month + " del año " + year;
+            }
+
+            public bool checkDate()
+            {
+
+
+                if (month <= 0 && month > 12)
+                {
+                    return false;
+                }
+                if (day < 0 && day > 31)
+                {
+                    return false;
+                }
+
+                switch (month)
+                {
+                    //meses de 30 dias
+                    case 4: if (day == 31) return false; break;
+                    case 6: if (day == 31) return false; break;
+                    case 9: if (day == 31) return false; break;
+                    case 11: if (day == 31) return false; break;
+                    //meses de 31
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        if (day == 31) return true; break;
+
+                    case 2:
+                        if (IsLeap() == true)
+                        {
+                            if (day > 29) //es bisiesto
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                if (day > 28) //no lo es
+                                {
+                                    return false;
+                                }
+
+                            }
+                        }
+                        break;
+
+                }
+                return true;
+            }
+
+            public bool IsLeap() //bisiesto
+            {
+
+                return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+            }
+
+            public void nextDay()
+            {
+                day = day + 1;
+                if (month == 11 || month == 4 || month == 6 || month == 9) //30 dias
+                {
+                    if (day >= 31)
+                    {
+                        day = 1;
                         month = month + 1;
                     }
 
                 }
-            }
-            if (month > 12)
-            {
+                else 
+                if (month == 2)
+                {
+                    if (day > 29) //es bisiesto
+                    {
+                        day = day - 29;
+                        month = month + 1;
+                    }
+                    else
+                    {
+                        if (day > 28) //no lo es
+                        {
+                            day = day - 28;
+                            month = month + 1;
+                        }
 
-                month = month - 12;
-                year = year + 1;
+                    }
+                }
+                if (month > 12)
+                {
+
+                    month = month - 12;
+                    year = year + 1;
+                }
+
             }
+
 
         }
-
-
     }
 }
+
